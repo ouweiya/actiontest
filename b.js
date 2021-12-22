efcd2497645dfcf5201b83f3556477e201075d70
ouweiya <higogle1@gmail.com>
2021-12-23 00:07:38 +0800

    email

--- b.js
+++ b.js
@@ -1,46 +1,41 @@
-// const fetch = require('node-fetch');
-// console.log('fetch: ', fetch);
-
-// console.log('Time', new Date().toLocaleTimeString());
-// (async () => {
-//   const data = await fetch('https://www.dual-subtitles.com/');
-//   console.log(data.json());
-// })();
-
-// const https = require('https');
-
-// const options = {
-//   hostname: 'www.dual-subtitles.com',
-//   port: 443,
-//   // path: '/todos',
-//   method: 'GET',
-// };
-
-// const req = https.request(options, res => {
-//   console.log(`statusCode: ${res.statusCode}`);
-
-//   res.on('data', d => {
-//     process.stdout.write(d);
-//   });
-// });
-
-// req.on('error', error => {
-//   console.error(error);
-// });
-
-// req.end();
 const axios = require('axios').default;
+const nodemailer = require('nodemailer');
 
 axios
-  .get('https://www.dual-subtitles.co/2')
+  .get('https://www.dual-subtitles.co/')
   .then(res => {
     console.log(res.status === 200);
   })
   .catch(({ response }) => {
-    // console.log(response.status, data);
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'salzburgrujvnun@gmail.com',
+        pass: 'fmqblvbnggpyjydx',
+      },
+    });
+    let errorMessage = '';
     if (response) {
-      console.log('服务器响应:', response.status);
+      errorMessage = `服务器响应: ${response.status}`;
     } else {
-      console.log('服务器无响应');
+      errorMessage = '服务器无响应!';
     }
+
+    const mailOptions = {
+      from: 'salzburgrujvnun@gmail.com',
+      to: 'higogle1@gmail.com',
+      subject: '⚠️ 网站异常提醒!',
+      html: `<div style="color: red">
+              <h2>${errorMessage}</h2>
+              <b>${new Date().toLocaleDateString('zh')} ${new Date().toLocaleTimeString('zh')}</b>
+            </div>`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
   });
